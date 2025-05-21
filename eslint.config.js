0d9b96dac659da289825b42cbaeaa7b28108a1ff
MarioGavin <154669431+MarioGavin@users.noreply.github.com>
2025-05-21 20:17:29 +0200

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -8,7 +8,7 @@ module.exports = [
             ],
             "indent": [
                 "error",
-                2
+                4
             ],
             "linebreak-style": [
                 "error",
